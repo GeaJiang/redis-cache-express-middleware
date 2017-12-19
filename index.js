a2d1098062e07420f812464faa2f3acc341f8c1f
gea <rightjiang@hotmail.com>
2017-12-19 20:42:27 +0800

    base structure

--- index.js
+++ index.js
@@ -1,8 +1,28 @@
 'use strict';
 
 var redis = require('redis');
+var bluebird = require('bluebird');
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
 
 //TODO: store quantity limit
 //TODO: different user
-//TODO: before res store
+//TODO: before res store override express responselancic
 //TODO: FIFO LRU LFU
+
+var default_structure = ['FIFO', 'LFU', 'LRU'];
+
+class RedisCache {
+
+  constructor(options) {
+    let { limit, structure, redis_db } = options;
+    this.limit = limit || 2000;
+    this.structure = ~default_structure.indexOf(structure) ? structure : 'LFU';
+    this.redis_db = redis_db || 1;
+  }
+
+  store(key, subkey) {
+    
+  }
+}
